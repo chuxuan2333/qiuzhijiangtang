3c825e216c9742a942f870167f5e3b15e0fc397f
chuxuan <1020389527@qq.com>
2021-02-16 22:50:38 +0800

    光遇乐谱制作

--- python-应用/光遇乐谱制作.py
+++ python-应用/光遇乐谱制作.py
@@ -1,4 +1,9 @@
 def change(s):
+    """
+    将歌曲简谱转化成sky A1-A5，B1-B5,C1-C5形式
+    :param s:
+    :return:
+    """
     dict1 = {
         'A1': 'A1', 'A2': 'A2', 'A3': 'A3', 'A4': 'A4', 'A5': 'A5',
         'A6': 'B1', 'A7': 'B2', 'B1': 'B3', 'B2': 'B4', 'B3': 'B5',
@@ -7,6 +12,7 @@ def change(s):
     if s in dict1.keys():
         return dict1[s]
     else:
+        return '*'
         pass
 
 
@@ -36,8 +42,8 @@ tiankongzhicheng = ['A6', 'A7', 'B1', 'A7', 'B1', 'B3', 'A7',
 
 
 '''
-# 简单的幸福副歌
-jiandandexingfu = ['A1' 'A2' 'A2' 'A4' 'A3'
+# 简单的幸福
+jiandandexingfu = [A1 A2 A2 A4 A3
                    A3 A3 A2 A3 A6 A5
                    A5 A5 A6 A6 A5 B1
                    B1 B2 B1 A3 A5 A5
@@ -45,7 +51,6 @@ jiandandexingfu = ['A1' 'A2' 'A2' 'A4' 'A3'
                    A6 A6 A5 A5 A5 A5A3
                    A3 A2 A1 A1 A3 A2 A3 A2
                    A6 A5 A6 A5
-
                    A1 A2 A2 A4 A3
                    A3 A3 A2 A3 A6 A5
                    A5 A5 A6 A6 A5 B1
@@ -53,7 +58,7 @@ jiandandexingfu = ['A1' 'A2' 'A2' 'A4' 'A3'
                    A5 A5 A6 A6 A5 B1 A6
                    A6 A6 A5 A5 A5 A5A3
                    A3 A2 A1 A2
-                   —————————  — — —
+                   —???——A6 A5 A6 A5
                    ]
 '''
 
@@ -69,7 +74,6 @@ planet = [B3 B3 B2 B5
           B4 B4 B4 B3 B2 B1 B2 B3 B2
           B5 B5 B5 B4 B3 B2 B1
           B6 B5 B6 B7
-
           B1 A7 A6 A5 A6 A5 A2 A5 A3
           B1 A7 A6 A5 A6 B2 B3 A6A6 A5 A6
           B1 A7 A6 A5 A6 A5 A2 A5 A3
@@ -78,6 +82,26 @@ planet = [B3 B3 B2 B5
           B1 B2B1 B1 A7 A6 A5 B1
           B1 B2B1 B1 A7 A6 A5 B1 A5
           B1 B2B1 B1 A7 A6 A5 B1
+          ]
+'''
+
+'''
+# 赤伶
+chiling = [
+           B3 B5 B2 B1 B2 B3 B3
+           A6 A7 B1 B1 B3 B2 B1 B2 B3
+           B3 B5 B2 B1 B2 B3 B2 B1
+           A6 A7 B1 B1 B3 B2 A5 A7 A6
+           A6 A7 B1 B2 B2 B3 B3 B5 B5 B6 B2
+           B2 B3 B5 B3 B3 B2 B1
+           A6 A6 A7 B1 B2 B3
+           A6 A7 B1 B2 B2 B3 B3 B5 B5 B6 B2
+           B2 B3 B5 B3 B3 B2 B1
+           A6 B6 B5 B3 B2 B3
+           B3 B2 B3 A6 B3 A5 A6 A7 A6 B1
+           B3 B2 B3 A6 B3 A5 A6 A7 B5 B3
+           B3 B2 B3 A6 B3 A5 A6 A7 A6 B1 B2 B3 A6
+           B2 B3 A6 B2 B3 B2 A5 A7 A6
           ]
 '''
 
